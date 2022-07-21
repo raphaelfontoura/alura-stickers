67f6b6fe5046b3ec4a20ca859b9c67521d327b62
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-07-21 01:59:11 -0300

    Refactor App

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
@@ -6,23 +8,36 @@ public class App {
 
   public static void main(String[] args) throws Exception {
 
-    String url = "https://api.mocki.io/v2/549a5d8b";
+    String urlFilmes = "https://api.mocki.io/v2/549a5d8b";
 
     var httpClient = new ClienteHttp();
-    String json = httpClient.buscaDados(url);
+    String jsonFilmes = httpClient.buscaDados(urlFilmes);
 
     ExtratorConteudo extratorFilmes = new ExtratorDeConteudoIMDB();
-    List<Conteudo> conteudosFilmes = extratorFilmes.extraiConteudos(json);
+    List<Conteudo> conteudosFilmes = extratorFilmes.extraiConteudos(jsonFilmes);
 
     GeradorDeStickers geradorDeStickers = new GeradorDeStickers();
 
-    for (Conteudo item : conteudosFilmes) {
-      InputStream inputStream = new URL(item.getUrlImagem()).openStream();
-      String nomeArquivo = item.getTitulo() + ".png";
-      geradorDeStickers.gerarSticker(inputStream, nomeArquivo, "Só Filme TOPZERA!!");
-      System.out.println(item);
-    }
+    generateStickersAndPrint(geradorDeStickers, conteudosFilmes, "Só filme TOPZERA!!");
 
+    String urlNasa = "https://api.mocki.io/v2/549a5d8b/NASA-APOD";
+    String jsonNasa = httpClient.buscaDados(urlNasa);
+
+    ExtratorConteudo extratorNasa = new ExtratorDeConteudoNasa();
+    List<Conteudo> conteudosNasa = extratorNasa.extraiConteudos(jsonNasa);
+
+    generateStickersAndPrint(geradorDeStickers, conteudosNasa, "Só foto LOKA!!");
+
+  }
+
+  private static void generateStickersAndPrint(GeradorDeStickers geradorDeStickers, List<Conteudo> conteudos, String frase)
+      throws IOException, MalformedURLException {
+    for (Conteudo conteudo : conteudos) {
+        InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
+        String nomeArquivo = conteudo.getTitulo() + ".png";
+        geradorDeStickers.gerarSticker(inputStream, nomeArquivo, frase);
+        System.out.println(conteudo);
+    }
   }
 
 }
